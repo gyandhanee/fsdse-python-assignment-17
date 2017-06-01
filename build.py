7370c2ab99b7cbb75592681895c5c62c1d9ffaf4
gyandhanee <gyandhanee@users.noreply.github.com>
2017-06-01 05:25:46 +0000

    Done

--- build.py
+++ build.py
@@ -2,13 +2,12 @@ import operator
 
 
 def solution_asc(dic):
-    '''
-    Enter your code here
-    '''
+    list_key_value = [ (k,v) for k, v in dic.items() ]
+    list_key_value.sort(key=operator.itemgetter(0))
+    return list_key_value
 
 
 def solution_desc(dic):
-    '''
-    Enter your code here
-    '''
-
+    list_key_value = [ (k,v) for k, v in dic.items() ]
+    list_key_value.sort(key=operator.itemgetter(0), reverse=True)
+    return list_key_value
